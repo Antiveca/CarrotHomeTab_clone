8e824193d05a0d0c417acfc6891b309afea452e1
antiveca <chlqud1252@gmail.com>
2024-02-05 21:36:05 +0900

    Home View 이미지 불러오기 - Kingfisher 라이브러리를 이용하여 이미지 불러옴

--- CarrotHomeTab/Home/ItemInfoCell.swift
+++ CarrotHomeTab/Home/ItemInfoCell.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Kingfisher
 
 class ItemInfoCell: UICollectionViewCell {
     @IBOutlet weak var thumbnail: UIImageView!
@@ -15,15 +16,27 @@ class ItemInfoCell: UICollectionViewCell {
     @IBOutlet weak var numOfChatLabel: UILabel!
     @IBOutlet weak var numOfLikeLabel: UILabel!
     
+    override func awakeFromNib() { //인터페이스가 보여진 후? 적용됨
+        super.awakeFromNib()
+        thumbnail.layer.cornerRadius = 10
+        thumbnail.layer.masksToBounds = true
+        thumbnail.tintColor = .white
+    }
+    
     func configure(item: ItemInfo) {
+        thumbnail.kf.setImage(
+            with: URL(string: item.thumbnailURL),
+            placeholder: UIImage(systemName: "hands.sparkles.fill") //placeholder는 로딩되지 않았을 때 출력될 이미지
+        )
+        
         titleLabel.text = item.title
         descriptionLabel.text = item.location
         priceLabel.text = "\(formatNumber(item.price))원"
         
-        
         numOfChatLabel.text = "\(item.numOfChats)"
         numOfLikeLabel.text = "\(item.numOfLikes)"
     }
+    
     
     private func formatNumber(_ price: Int) -> String { //숫자를 가격을 확인하기 쉽게 쉼표를 표시
         let numFomat = NumberFormatter()
